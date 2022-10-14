cdb8f81c4c3b6f584e167ff13ff5e664a7ede0d5
csae8092 <peter.andorfer@oeaw.ac.at>
2022-10-14 09:58:38 +0200

    added mgm-command to harmonize domains #2

---
+++ dumper/management/commands/fix_domains.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from apis_core.apis_metainfo.models import Uri
+from dumper.utils import DOMAIN_MAPPING
+
+
+class Command(BaseCommand):
+    help = 'Command to harmonize URL-Domains'
+
+    def handle(self, *args, **kwargs):
+        print("start fixing domains")
+        domains = [x[1] for x in DOMAIN_MAPPING]
+        wrong_domain = Uri.objects.exclude(domain__in=domains)
+        print(f"found {wrong_domain.count()} with wrong or without domains")
+        print("start fixing domains")
+        for x in DOMAIN_MAPPING:
+            to_fix = wrong_domain.filter(uri__icontains=x[0])
+            print(x[1])
+            for uri in tqdm(to_fix, total=to_fix.count()):
+                uri.domain = x[1]
+                uri.save()
+        print("done fixing domains")
+
+        wrong_domain = Uri.objects.exclude(domain__in=domains)
+        print(f"no I found {wrong_domain.count()} with wrong or without domains")
