f9fb666d38a4b0c369d65193a31db6906cc98a51
csae8092 <peter.andorfer@oeaw.ac.at>
2023-12-09 11:37:13 +0100

    log-in and out works again

--- dumper/views.py
+++ dumper/views.py
@@ -1,6 +1,11 @@
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from apis_core.apis_entities.models import Person, Place, Event, Work, Institution
 from apis_core.apis_metainfo.models import Uri
+
+from .forms import form_user_login
 
 
 class HomePageView(TemplateView):
@@ -16,3 +21,23 @@ class HomePageView(TemplateView):
         context["institution_count"] = Institution.objects.all().count()
         context["uri_count"] = Uri.objects.all().count()
         return context
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = form_user_login(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd["username"], password=cd["password"])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(request.GET.get("next", "/"))
+            return HttpResponse("user does not exist")
+    else:
+        form = form_user_login()
+        return render(request, "dumper/user_login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, "dumper/user_logout.html")
