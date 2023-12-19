c122288c2cb9969b71a8e861b8fa8b6341c10e43
csae8092 <peter.andorfer@oeaw.ac.at>
2023-12-19 12:04:26 +0100

    first try with test wf

--- apis_core/apis_entities/tests.py
+++ apis_core/apis_entities/tests.py
@@ -1,1 +1,28 @@
-# Create your tests here.
+# from django.apps import apps
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
+from apis_core.apis_entities.models import Person
+
+
+client = Client()
+USER = {"username": "testuser", "password": "somepassword"}
+
+
+class EntitiesTestCase(TestCase):
+    fixtures = [
+        "db.json",
+    ]
+
+    def setUp(self):
+        # Create two users
+        User.objects.create_user(**USER)
+
+    def test_001_person_list_view(self):
+        url = Person.get_listview_url()
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_002_check_fixtures(self):
+        items = Person.objects.all().count()
+        self.assertEqual(items, 2)
