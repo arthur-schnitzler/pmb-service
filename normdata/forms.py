a3e2a454761e376003100843c546875c5791ad70
csae8092 <peter.andorfer@oeaw.ac.at>
2023-12-28 11:41:44 +0100

    wip: refactoring of GndFormView

--- normdata/forms.py
+++ normdata/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class GndForm(forms.Form):
     gnd_url = forms.URLField(
         label="Normdata URL",
-        help_text="Zum Beispiel: http://lobid.org/gnd/118566512",
+        help_text="Zum Beispiel: http://lobid.org/gnd/118566512 oder https://www.geonames.org/2772400/linz.html",
         max_length=100,
     )
     entity_type = forms.ChoiceField(
--- normdata/views.py
+++ normdata/views.py
@@ -1,12 +1,9 @@
-from .forms import GndForm
-from django.views.generic.edit import FormView
 from django.urls import reverse
-from apis_core.apis_entities.models import Person, Uri
-from django.core.exceptions import ObjectDoesNotExist
-from pylobid.pylobid import PyLobidPerson
-from AcdhArcheAssets.uri_norm_rules import get_normalized_uri
-from acdh_id_reconciler import gnd_to_wikidata
-from acdh_wikidata_pyutils import WikiDataPerson
+from django.views.generic.edit import FormView
+from icecream import ic
+
+from .forms import GndForm
+from .utils import import_from_normdata
 
 
 class GndFormView(FormView):
@@ -17,41 +14,9 @@ class GndFormView(FormView):
         return reverse("apis:apis_entities:person_list_view")
 
     def form_valid(self, form):
-        raw_gnd_url = form.data["gnd_url"]
-        gnd_id = get_normalized_uri(raw_gnd_url)
-        try:
-            Uri.objects.get(uri=gnd_id)
-            return super().form_valid(form)
-        except ObjectDoesNotExist:
-            pass
-        try:
-            wikidata_id = gnd_to_wikidata(gnd_id)["wikidata"]
-        except (IndexError, KeyError):
-            wikidata_id = False
-        if wikidata_id:
-            try:
-                Uri.objects.get(uri=get_normalized_uri(wikidata_id))
-                return super().form_valid(form)
-            except ObjectDoesNotExist:
-                wd_person = WikiDataPerson(wikidata_id)
-                apis_person = wd_person.get_apis_entity()
-                person = Person.objects.create(**apis_person)
-                Uri.objects.create(
-                    uri=get_normalized_uri(wikidata_id),
-                    domain="wikidata",
-                    entity=person,
-                )
-                Uri.objects.create(uri=gnd_id, domain="gnd", entity=person)
-            return super().form_valid(form)
-        else:
-            py_ent = PyLobidPerson(gnd_id, fetch_related=False)
-            person_dict = {}
-            if ", " in py_ent.pref_name:
-                person_dict["name"], person_dict["first_name"] = py_ent.pref_name.split(
-                    ", "
-                )
-            else:
-                person_dict["name"] = py_ent.pref_name
-            person = Person.objects.create(**person_dict)
-            Uri.objects.create(uri=gnd_id, domain="gnd", entity=person)
-            return super().form_valid(form)
+        ic(form.data)
+        raw_url = form.data["gnd_url"]
+        entity_type = form.data["entity_type"]
+        entity = import_from_normdata(raw_url, entity_type)
+        ic(entity)
+        return super().form_valid(form)
