6eab6a369c1934ba0db28884a2fd33b132ebd226
csae8092 <peter.andorfer@oeaw.ac.at>
2023-12-28 15:06:50 +0100

    tests for archemd module

--- archemd/urls.py
+++ archemd/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import entity_as_arche
 
+app_name = "archemd"
+
 urlpatterns = [
-    path("<int:pk>", entity_as_arche),
+    path("<int:pk>", entity_as_arche, name="arche"),
 ]
