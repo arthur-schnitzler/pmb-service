e0d0f118ffec68703dafaaabda950d5c5073832b
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-01 16:49:06 +0100

    test dumper views

--- dumper/tests.py
+++ dumper/tests.py
@@ -5,13 +5,14 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.test import Client, TestCase
+from django.urls import reverse
 
 from apis_core.apis_entities.models import Person
 from apis_core.apis_metainfo.models import Uri
 from normdata.utils import import_from_normdata
 
 client = Client()
-USER = {"username": "testuser", "password": "somepassword"}
+USER = {"username": "testuser", "password": "password", "is_active": True}
 BAHR = {"name": "Bahr", "first_name": "Hermann", "start_date_written": "1900"}
 
 ENTITY_TYPES = ["person", "place", "event", "work", "institution"]
@@ -60,3 +61,22 @@ class DumperTestCase(TestCase):
         call_command("wikidata_minter")
         after_uris = Uri.objects.all().count()
         self.assertEqual(before_uris, after_uris)
+
+    def test_05_startpage(self):
+        url = reverse("dumper:home")
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_06_login_and_out(self):
+        rv = self.client.get(reverse("dumper:home"))
+        self.assertEqual(rv.status_code, 200)
+        self.assertContains(rv, "Personen der Moderne Basis")
+        rv = self.client.get(reverse("dumper:user_login"))
+        self.assertContains(rv, "Username")
+        form_data = {"username": "temporary", "password": "temporary"}
+        rv = self.client.post(reverse("dumper:user_login"), form_data, follow=True)
+        rv = self.client.get(reverse("dumper:user_logout"), follow=True)
+        self.assertContains(rv, "Küss die Hand")
+        form_data = {"username": "whatever", "password": USER["password"]}
+        rv = self.client.post(reverse("dumper:user_login"), form_data, follow=True)
+        self.assertContains(rv, "user does not exist")
