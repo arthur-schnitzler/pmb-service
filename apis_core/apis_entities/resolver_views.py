8bb78a3e500d63615233503c02bbc1724b8757df
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-04 07:55:30 +0100

    params for uri-resolver

--- apis_core/apis_entities/resolver_views.py
+++ apis_core/apis_entities/resolver_views.py
@@ -8,8 +8,7 @@ from apis_core.apis_metainfo.models import TempEntityClass, Uri
 
 def uri_resolver(request):
     uri = request.GET.get("uri", None)
-    # format_param = request.GET.get("format", False)
-    # requested_format = request.META.get("HTTP_ACCEPT")
+    format_param = request.GET.get("format", False)
     if uri is None:
         raise Http404
     else:
@@ -18,7 +17,19 @@ def uri_resolver(request):
         except ObjectDoesNotExist:
             raise Http404
         entity = TempEntityClass.objects_inheritance.get_subclass(pk=uri.entity_id)
-        url = entity.get_absolute_url()
+        if format_param:
+            if format_param == "tei":
+                try:
+                    url = entity.get_tei_url()
+                except (NoReverseMatch, AttributeError):
+                    raise Http404
+            elif format_param == "json":
+                try:
+                    url = entity.get_api_url()
+                except NoReverseMatch:
+                    raise Http404
+            else:
+                raise Http404
         return redirect(url)
 
 
