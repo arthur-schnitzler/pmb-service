7f74a152fb9d5e1f15beeec6c9ba5eefb33ba7dd
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-11 17:08:34 +0100

    added imprint and about views

--- dumper/views.py
+++ dumper/views.py
@@ -4,9 +4,42 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 from apis_core.apis_entities.models import Event, Institution, Person, Place, Work
+from django.conf import settings
 from apis_core.apis_metainfo.models import Uri
+import requests
+
 
 from .forms import form_user_login
+
+
+def get_imprint_url():
+    base_url = "https://imprint.acdh.oeaw.ac.at/"
+    redmine_id = getattr(settings, "REDMINE_ID", "go-register-a-redmine-service-issue")
+    return f"{base_url}{redmine_id}"
+
+
+class AboutView(TemplateView):
+    template_name = "dumper/about.html"
+
+
+class ImprintView(TemplateView):
+    template_name = "dumper/imprint.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        r = requests.get(get_imprint_url())
+
+        if r.status_code == 200:
+            context["imprint_body"] = f"{r.text}"
+        else:
+            context[
+                "imprint_body"
+            ] = """
+            On of our services is currently not available. Please try it later or write
+            an email to acdh@oeaw.ac.at; if you are service provide, make sure that you
+            provided ACDH_IMPRINT_URL and REDMINE_ID
+            """
+        return context
 
 
 class HomePageView(TemplateView):
