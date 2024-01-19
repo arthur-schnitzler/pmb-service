84d7b49ac370abc14d8a09d494195a6915caf717
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-19 09:53:05 +0100

    minor [skip ci]

--- dump_and_restore.sh
+++ dump_and_restore.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pg_dump -d pmb -h localhost -p 5432 -U  postgres -c -f pmb_dump.sql
-pg_dump -d pmb_play -h localhost -p 5432 -U  postgres -c -f pmb_play_dump.sql
-
+pg_dump -d pmb -h localhost -p 5433 -U  pmb -c -f pmb_dump.sql
+psql -U postgres -d pmb < pmb_dump.sql
