aedc200960cf1b8ce9b14a63901880575555374f
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-23 08:48:54 +0100

    na bitte, geht doch

--- apis_core/apis_entities/tests.py
+++ apis_core/apis_entities/tests.py
@@ -370,9 +370,8 @@ class EntitiesTestCase(TestCase):
         entity = import_from_normdata(grillparzer, "person")
         entity.fetch_image()
         self.assertTrue(entity.img_url)
-        self.assertTrue("Wikimedia Commons", entity.img_credit)
+        self.assertTrue("Wikimedia Commons" in entity.img_credit())
 
     def test_027_img_credit(self):
         entity = import_from_normdata("https://www.wikidata.org/wiki/Q76483", "person")
-        self.assertTrue(entity.img_url)
-        self.assertIsNone(entity.img_credit)
+        self.assertIsNone(entity.img_credit())
