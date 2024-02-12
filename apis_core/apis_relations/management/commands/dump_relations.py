4869a0d36d0a5032b1a47d0bcb385bfb6c9d6b69
csae8092 <peter.andorfer@oeaw.ac.at>
2024-02-12 15:39:45 +0100

    mgm cmd to find delete duplicated relations added

--- apis_core/apis_relations/management/commands/dump_relations.py
+++ apis_core/apis_relations/management/commands/dump_relations.py
@@ -1,14 +1,16 @@
 import os
 import pandas as pd
+import recordlinkage
+
 from datetime import datetime
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
+from icecream import ic
 from tqdm import tqdm
 from typing import Any
 from apis_core.apis_relations.models import AbstractRelation
-from dumper.utils import write_report
+from dumper.utils import upload_files_to_owncloud, write_report
 
 
 class Command(BaseCommand):
@@ -24,14 +26,47 @@ class Command(BaseCommand):
             print(x.__name__)
             for y in tqdm(x.objects.all()):
                 try:
-                    data.append(y.get_web_object())
+                    item = y.get_web_object()
+                    item["relation_pk"] = y.id
+                    data.append(item)
                 except AttributeError:
                     issues.append(y)
         df = pd.DataFrame(data)
+        print("lets find and delete duplicated relations")
+        df.set_index("relation_pk", inplace=True, drop=False)
+        save_path = os.path.join(settings.MEDIA_ROOT, "relations.csv")
+        print(f"serialized {len(df)} relations")
+        df.to_csv(save_path, index=False)
+
+        df = pd.read_csv(save_path).fillna("nodate")
+        df.set_index("relation_pk", inplace=True, drop=False)
+        indexer = recordlinkage.Index()
+        indexer.block(["relation_type", "source_id", "target_id", "start_date", "end_date"])
+        duplicates = indexer.index(df)
+        print(f"deleting {len(duplicates)} duplicated relations")
+
+        deleted = []
+        for double in duplicates:
+            for x in AbstractRelation.get_all_relation_classes():
+                try:
+                    item = x.objects.get(id=double[1])
+                except:  # noqa
+                    continue
+                deleted.append(item.id)
+                item.delete()
+                break
+        print(deleted)
+        df.drop(deleted)
         save_path = os.path.join(settings.MEDIA_ROOT, "relations.csv")
         df.to_csv(save_path, index=False)
         end_time = datetime.now().strftime(settings.PMB_TIME_PATTERN)
         report = [os.path.basename(__file__), start_time, end_time]
         write_report(report)
         print(f"serialized {len(df)} relations")
+        files = list()
+        files.append(save_path)
+        try:
+            upload_files_to_owncloud(files)
+        except Exception as e:
+            ic(e)
         return "done"
