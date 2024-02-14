0ae9e94d049aa8f953c961847e9f68e4557b3635
csae8092 <peter.andorfer@oeaw.ac.at>
2024-02-14 17:03:05 +0100

    compare coordinates for duplicates places, see https://github.com/arthur-schnitzler/pmb-service/issues/147

--- apis_core/apis_entities/management/commands/find_duplicated_places.py
+++ apis_core/apis_entities/management/commands/find_duplicated_places.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import recordlinkage
+from recordlinkage.compare import Geographic
 
 from typing import Any
 from django.conf import settings
@@ -18,11 +19,13 @@ class Command(BaseCommand):
         props = [
             "id",
             "name",
+            "lat",
+            "lng"
         ]
         df = pd.DataFrame(
             Place.objects.values_list(*props),
             columns=props,
-        ).astype("str")
+        ).astype("str").fillna("nix")
         df["custom_index"] = df["id"].astype(str) + " " + df["name"]
         df.set_index("custom_index", inplace=True)
         indexer = recordlinkage.Index()
@@ -31,8 +34,10 @@ class Command(BaseCommand):
         len(candidate_links)
         compare_cl = recordlinkage.Compare()
         compare_cl.exact("name", "name", label="name")
+        compare_cl.exact("lat", "lat", label="lat")
+        compare_cl.exact("lng", "lng", label="lng")
         features = compare_cl.compute(candidate_links, df)
-        matches = features[features.sum(axis=1) > 0]
+        matches = features[features.sum(axis=1) > 2]
         save_path = os.path.join(settings.MEDIA_ROOT, "duplicated_places.csv")
         matches.to_csv(save_path)
         print(f"found {len(matches)} potential duplicates")
