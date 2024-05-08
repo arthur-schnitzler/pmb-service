97e4819624680dce2d69ac2a09654f94ddd38e9d
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-08 14:00:06 +0200

    objects_ingeritance restricted to entity classes

--- apis_core/utils.py
+++ apis_core/utils.py
@@ -10,21 +10,23 @@ def get_object_from_pk_or_uri(pk):
     """checks if the given pk exists, if not checks if a matching apis-default uri exists
     and returns its entity"""
     try:
-        instance = TempEntityClass.objects_inheritance.get_subclass(pk=pk)
+        instance = TempEntityClass.objects_inheritance.select_subclasses(
+            "place", "person", "work", "event", "institution"
+        ).get(pk=pk)
         return instance
     except ObjectDoesNotExist:
         domain = "https://pmb.acdh.oeaw.ac.at/"
         new_uri = f"{domain}entity/{pk}/"
         uri2 = Uri.objects.filter(uri=new_uri)
         if uri2.count() == 1:
-            instance = TempEntityClass.objects_inheritance.get_subclass(
-                pk=uri2[0].entity_id
-            )
+            instance = TempEntityClass.objects_inheritance.select_subclasses(
+                "place", "person", "work", "event", "institution"
+            ).get(pk=uri2[0].entity_id)
         elif uri2.count() == 0:
             temp_obj = get_object_or_404(Uri, uri=new_uri[:-1])
-            instance = TempEntityClass.objects_inheritance.get_subclass(
-                pk=temp_obj.entity_id
-            )
+            instance = TempEntityClass.objects_inheritance.select_subclasses(
+                "place", "person", "work", "event", "institution"
+            ).get(pk=temp_obj.entity_id)
         else:
             raise Http404
         return instance
