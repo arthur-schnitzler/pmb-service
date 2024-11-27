a3d3d7af6a0246097500282732965b7a08aab2a5
csae8092 <p.andorfer@gmail.com>
2024-11-27 09:36:01 +0100

    strange webgl not supported error [skip ci]

--- static/src/js/lib.js
+++ static/src/js/lib.js
@@ -1,9 +1,28 @@
 export function getSpaceSize(nodeCount) {
-    if (nodeCount < 100) {
-      return 4096 / 2;
-    } else if (nodeCount > 99 && nodeCount < 10000) {
-      return 4096;
-    } else {
-      return 4096 * 2;
-    }
+  if (nodeCount < 100) {
+    return 4096 / 2;
+  } else if (nodeCount > 99 && nodeCount < 10000) {
+    return 4096;
+  } else {
+    return 4096 * 2;
   }
+}
+export const COLORS = {
+  person: "#720e07",
+  place: "#5bc0eb",
+  work: "#ff8600",
+  event: "#9bc53d",
+  institution: "#ffdd1b",
+};
+
+export function pause() {
+  isPaused = true;
+  pauseButton.textContent = "Start";
+  graph.pause();
+}
+
+export function start() {
+  isPaused = false;
+  pauseButton.textContent = "Pause";
+  graph.start();
+}
--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,26 +1,15 @@
-import { Cosmograph, CosmographTimeline } from "@cosmograph/cosmograph";
-import { getSpaceSize } from './lib.js'
+import { Graph } from "@cosmograph/cosmos";
+import { getSpaceSize, COLORS } from "./lib.js";
 
 async function init() {
-  const alertNode = document.getElementById("alert");
   const spinnerNode = document.getElementById("spinner");
-  const legendNode = document.getElementById("legend");
-  const canvas = document.createElement("div");
-
+  const alertNode = document.getElementById("alertNode");
   const queryString = window.location.search;
   const url = `/network/csv/${queryString}`;
 
   try {
     const res = await fetch(url);
     const data = await res.json();
-
-    const colors = {
-      person: "#720e07",
-      place: "#5bc0eb",
-      work: "#ff8600",
-      event: "#9bc53d",
-      institution: "#ffdd1b",
-    };
 
     const links = data["edges"].map((d) => ({
       source: parseInt(d.s),
@@ -31,10 +20,8 @@ async function init() {
     const nodes = data["nodes"].map((d) => ({
       id: parseInt(d.id),
       label: d.l,
-      color: colors[d["k"]],
+      color: COLORS[d["k"]],
     }));
-
-    
 
     // Calculate node size
     const nodeDegrees = {};
@@ -47,49 +34,39 @@ async function init() {
     nodes.forEach((node) => {
       node.degree = nodeDegrees[node.id] || 0;
     });
-
-    const appNode = document.getElementById("app");
-
     // Remove spinner
     spinnerNode.classList.add("visually-hidden");
-    legendNode.style.visibility = "visible";
-    alertNode.classList.add("visually-hidden");
-
-    appNode.appendChild(canvas);
-    const searchContainer = document.createElement("div");
-    appNode.appendChild(searchContainer);
-
-    const config = {
-      spaceSize: getSpaceSize(nodes.length),
-      nodeColor: (d) => d.color,
+    let graph;
+    let config = {
+      backgroundColor: "#151515",
       nodeSize: (node) => {
         const degree = node.degree || 1; // Default to 1 if degree is not defined
         return Math.max(1, Math.log(degree * 10)); // Adjust the multiplier and minimum size as needed
       },
-      nodeLabelAccessor: (d) => d.label,
-      showTopLabels: false,
-      showDynamicLabels: false,
-      linkGreyoutOpacity: 0,
-      nodeLabelColor: "white",
-      hoveredNodeLabelColor: "white",
-      linkWidth: 1,
+      nodeColor: (d) => d.color,
+      nodeGreyoutOpacity: 0.1,
+      linkWidth: 0.1,
+      linkColor: "#5F74C2",
       linkArrows: false,
-      onClick: (data) => alert(data.label),
-      simulationRepulsion: 1,
-      linkDistance: 5,
-      gravity: 0.5,
+      linkGreyoutOpacity: 0,
+      curvedLinks: true,
+      renderHoveredNodeRing: true,
+      hoveredNodeRingColor: "#4B5BBF",
+      simulation: {
+        linkDistance: 10,
+        linkSpring: 2,
+        repulsion: 0.2,
+        gravity: 0.1,
+        decay: 100000,
+      },
     };
-
-    const cosmograph = new Cosmograph(canvas, config);
-
-    const timelineContainer = document.createElement("div");
-    const timeline = new CosmographTimeline(cosmograph, timelineContainer);
-    cosmograph.setData(nodes, links);
-    appNode.appendChild(timelineContainer);
+    graph = new Graph(canvas, config);
+    graph.setData(nodes, links);
+    graph.zoom(0.9);
   } catch (error) {
     console.error("Failed to fetch data:", error);
-    alertNode.textContent = "Failed to load data. Please try again later.";
-    alertNode.style.visibility = "visible";
+    alertNode.textContent = "Daten konnten leider nicht geladen werden";
+    alertNode.classList.remove("visually-hidden");
   }
 }
 
