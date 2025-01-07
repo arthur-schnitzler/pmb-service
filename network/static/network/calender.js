672e78b273b5a54de3e9c19f301892782ecebb22
csae8092 <p.andorfer@gmail.com>
2025-01-07 14:37:39 +0100

    adds a slider to adjust the hex-radius

--- network/static/network/calender.js
+++ network/static/network/calender.js
@@ -1,21 +1,31 @@
 const url = document.getElementById("url").textContent;
 
 function showModal(object) {
-  const options = {focus: true, keyboard: true, backdrop: true, dismiss: true};
+  const options = {
+    focus: true,
+    keyboard: true,
+    backdrop: true,
+    dismiss: true,
+  };
   const eventLabels = object.points.map((p) => p.source.label);
   const curDate = object.points[0].source.date;
   const listItems = eventLabels.map((label) => `<li>${label}</li>`).join("");
   const urlParams = new URLSearchParams(window.location.search);
   urlParams.set("start_date", curDate);
   const newUrl = `/network/edges/?${urlParams.toString()}`;
-  document.getElementById("staticBackdropLabel").innerHTML = `<a href="${newUrl}">${curDate}</a>`;
+  document.getElementById(
+    "staticBackdropLabel"
+  ).innerHTML = `<a href="${newUrl}">${curDate}</a>`;
   document.getElementById("modal-body").innerHTML = `<ul>${listItems}</ul>`;
-  const myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), options);
+  const myModal = new bootstrap.Modal(
+    document.getElementById("staticBackdrop"),
+    options
+  );
   myModal.toggle();
 }
 
 console.log("fetching data");
-document.getElementById("loading-spinner").style.display = "block"; 
+document.getElementById("loading-spinner").style.display = "block";
 fetch(url)
   .then((response) => {
     if (!response.ok) {
@@ -49,6 +59,32 @@ fetch(url)
       }));
 
     console.log(validEvents);
+
+    let hexLayer = new deck.HexagonLayer({
+      id: "hexagon-layer",
+      data: validEvents,
+      getPosition: (d) => [d.longitude, d.latitude],
+      radius: 20000,
+      elevationScale: 4000,
+      elevationRange: [0, 50],
+      extruded: true,
+      pickable: true,
+      onClick: (object) => showModal(object.object),
+      onHover: ({ object, x, y }) => {
+        const tooltip = document.getElementById("tooltip");
+        if (object) {
+          const curDate = object.points[0].source.date;
+
+          tooltip.style.display = "block";
+          tooltip.style.left = `${x}px`;
+          tooltip.style.top = `${y}px`;
+          tooltip.innerHTML = `<strong>${curDate}</strong><p>Klicke um mehr zu sehen</p>`;
+        } else {
+          tooltip.style.display = "none";
+        }
+      },
+    });
+
     const deckgl = new deck.DeckGL({
       container: "map",
       initialViewState: {
@@ -58,37 +94,39 @@ fetch(url)
         latitude: 35,
         zoom: 2,
         pitch: 60,
-        // bearing: -1.7
       },
       controller: true,
-      // onViewStateChange: ({ viewState }) => {
-      //   console.log("Current view state:", viewState);
-      // },
-      layers: [
-        new deck.HexagonLayer({
-          data: validEvents,
-          getPosition: (d) => [d.longitude, d.latitude],
-          radius: 50000,
-          elevationScale: 4000,
-          elevationRange: [0, 50],
-          extruded: true,
-          pickable: true,
-          onClick: (object) => showModal(object.object),
-          onHover: ({ object, x, y }) => {
-            const tooltip = document.getElementById("tooltip");
-            if (object) {
-              const curDate = object.points[0].source.date;
-              
-              tooltip.style.display = "block";
-              tooltip.style.left = `${x}px`;
-              tooltip.style.top = `${y}px`;
-              tooltip.innerHTML = `<strong>${curDate}</strong><p>Klicke um mehr zu sehen</p>`;
-            } else {
-              tooltip.style.display = "none";
-            }
-          },
-        }),
-      ],
+      layers: [hexLayer],
+    });
+
+    document.getElementById("radiusSlider").addEventListener("input", (event) => {
+      const radius = event.target.value;
+      document.getElementById("radiusValue").textContent = radius;
+      hexLayer = new deck.HexagonLayer({
+        id: "hexagon-layer",
+        data: validEvents,
+        getPosition: (d) => [d.longitude, d.latitude],
+        radius: Number(radius),
+        elevationScale: 4000,
+        elevationRange: [0, 50],
+        extruded: true,
+        pickable: true,
+        onClick: (object) => showModal(object.object),
+        onHover: ({ object, x, y }) => {
+          const tooltip = document.getElementById("tooltip");
+          if (object) {
+            const curDate = object.points[0].source.date;
+
+            tooltip.style.display = "block";
+            tooltip.style.left = `${x}px`;
+            tooltip.style.top = `${y}px`;
+            tooltip.innerHTML = `<strong>${curDate}</strong><p>Klicke um mehr zu sehen</p>`;
+          } else {
+            tooltip.style.display = "none";
+          }
+        },
+      });
+      deckgl.setProps({ layers: [hexLayer] });
     });
 
     legendDiv.appendChild(dl);
@@ -99,7 +137,7 @@ fetch(url)
   });
 
 // Add this CSS for the tooltip
-const style = document.createElement("style");
+const style = document.createElement('style');
 style.innerHTML = `
   #tooltip {
     position: absolute;
@@ -113,6 +151,6 @@ style.innerHTML = `
 document.head.appendChild(style);
 
 // Add this HTML for the tooltip
-const tooltip = document.createElement("div");
-tooltip.id = "tooltip";
+const tooltip = document.createElement('div');
+tooltip.id = 'tooltip';
 document.body.appendChild(tooltip);
