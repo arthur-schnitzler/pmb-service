8d631a58dd03a7c44d68d7d5e50a9a5c8cc4f285
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2025-09-18 14:00:30 +0200

    chore(refactor): update_schnitzler_uris more generic [skip ci] (#368)

--- dumper/management/commands/update_schnitzler_uris.py
+++ dumper/management/commands/update_schnitzler_uris.py
@@ -8,10 +8,23 @@ from apis_core.apis_metainfo.models import Uri, TempEntityClass
 class Command(BaseCommand):
     help = "updates schnitzler uris"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--url',
+            type=str,
+            default="https://raw.githubusercontent.com/arthur-schnitzler/schnitzler-briefe-data/refs/heads/main/csv/uris-in-use.csv",  # noqa: E501
+            help="CSV URL to use"
+        )
+
     def handle(self, *args, **kwargs):
         print("start updating uris")
-        df = pd.read_csv("https://raw.githubusercontent.com/arthur-schnitzler/schnitzler-briefe-data/refs/heads/main/csv/uris-in-use.csv")  # noqa
-        to_delete = Uri.objects.filter(domain="schnitzler-briefe")
+        url = kwargs['url']
+        print(f"Using URL: {url}")
+        df = pd.read_csv(url)
+        domains = df["domain"].unique().tolist()
+        print(f"Found domains: {domains}")
+        to_delete = Uri.objects.filter(domain__in=domains)
+        print(f"found {to_delete.count()} in {" ".join([x for x in domains])} to delete")
         delete = to_delete.delete()
         print(f"deleted: {delete}")
         errors = []
