8efa44a8e575bbdcc5b496ca591abda082aa270f
csae8092 <p.andorfer@gmail.com>
2026-03-27 13:26:35 +0100

    chore: better secret key handling

--- pmb/settings.py
+++ pmb/settings.py
@@ -8,10 +8,8 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vz%#&+qlze@#cfc8aakgx2vy@k^cl)$$y%te#&q%u*_vd&rwlt"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("DEBUG"):
     DEBUG = True
     VITE_DEV = True
@@ -343,7 +341,7 @@ DOMAIN_MAPPING = [
     ("wienerschnitzler.", "wienerschnitzler", "#6F5106"),
     ("schnitzler-kultur.", "schnitzler-kultur", "#AC7790"),
     ("briefedition.wedekind.", "wedekind-korrespondenz", "#d4a346"),
-    ("https://biblio.ub.uni-freiburg.de/sf/#", "schnitzler-fischer", "#3D4F9F")
+    ("https://biblio.ub.uni-freiburg.de/sf/#", "schnitzler-fischer", "#3D4F9F"),
 ]
 
 
